a4a474a819f25de97476555434b1588a42189305
Sravan Kumar <bommana@iloguest.org>
2024-04-04 15:23:28 +0530

    added stereo type annotations

--- springcore/src/main/java/com/sravan/spring/springcore/App.java
+++ springcore/src/main/java/com/sravan/spring/springcore/App.java
@@ -1,15 +1,14 @@
 package com.sravan.spring.springcore;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App 
 {
     public static void main( String[] args )
     {
-       AbstractApplicationContext context = new ClassPathXmlApplicationContext("com/sravan/spring/springcore/config.xml");
-       MyDAO myDAO = (MyDAO)context.getBean("myDAO");
-       System.out.println(myDAO.toString());
+      ApplicationContext context = new ClassPathXmlApplicationContext("com/sravan/spring/springcore/config.xml");
+      Instructor instructor = context.getBean("instructor", Instructor.class);
+      System.out.println(instructor);
     }
 }
